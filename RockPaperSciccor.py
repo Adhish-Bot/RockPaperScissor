7864aad547fc3bfaed794cb16d6c0b5569a5824f
Adhish <adhish020@gmail.com>
2026-03-25 19:05:37 -0700

    Implement Rock-Paper-Scissor game logic

--- RockPaperSciccor.py
+++ RockPaperSciccor.py
@@ -1,1 +1,32 @@
 
+import random
+
+computerChoices = ["Rock","Paper","Scissor"]
+
+userChoices = input("1. Rock \n2. Paper \n3. Scissor \nEnter one of these options:")
+
+randomChoice = random.choice(computerChoices)
+
+if userChoices.title() == "Rock" and randomChoice == "Paper":
+    print(f"Your choice: {userChoices.title()} \nBot choice: {randomChoice}")
+    print("You Loose!")
+elif userChoices.title() == "Rock" and randomChoice == "Scissor":
+    print(f"Your choice: {userChoices.title()} \nBot choice: {randomChoice}")
+    print("You Win!")
+elif userChoices.title() == "Paper" and randomChoice == "Rock":
+    print(f"Your choice: {userChoices.title()} \nBot choice: {randomChoice}")
+    print("You Win!")
+elif userChoices.title() == "Paper" and randomChoice == "Scissor":
+    print(f"Your choice: {userChoices.title()} \nBot choice: {randomChoice}")
+    print("You Loose!")
+elif userChoices.title() == "Scissor" and randomChoice == "Rock":
+    print(f"Your choice: {userChoices.title()} \nBot choice: {randomChoice}")
+    print("You Loose!")
+elif userChoices.title() == "Scissor" and randomChoice == "Paper":
+    print(f"Your choice: {userChoices.title()} \nBot choice: {randomChoice}")
+    print("You Win!")
+elif (userChoices.title() == "Rock" and randomChoice == "Rock") or (userChoices == "Scissor" and randomChoice == "Scissor") or (userChoices == "Paper" and randomChoice == "Paper"):
+    print(f"Your choice: {userChoices.title()} \nBot choice: {randomChoice}")
+    print("Its a Tie!")
+else:
+    print("Please enter one of the following options!")
